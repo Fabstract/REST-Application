a4f4cd24ad5fd11de152ba65fd464a8374c1d059
Necip Allef <necipallef@gmail.com>
2018-03-19 12:07:21 +0300

    ADD AssertionException class

---
+++ src/Exception/AssertionException.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Fabstract\Component\REST\Exception;
+
+use Fabs\Component\Assert\AssertionExceptionInterface;
+
+class AssertionException extends Exception implements AssertionExceptionInterface
+{
+
+}
