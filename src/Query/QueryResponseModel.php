a5582dc21aeed7776956362b55f11e5f8ceff51c
Necip Allef <necipallef@gmail.com>
2018-03-26 11:11:44 +0300

    ADD getters, setters, asserts

--- src/Query/QueryResponseModel.php
+++ src/Query/QueryResponseModel.php
@@ -2,10 +2,15 @@
 
 namespace Fabstract\Component\REST\Query;
 
+use Fabstract\Component\REST\Assert;
+use Fabstract\Component\Serializer\Normalizer\NormalizableInterface;
+
 class QueryResponseModel
 {
-    public $model_list = [];
-    public $total_count = 0;
+    /** @var NormalizableInterface[] */
+    private $model_list = [];
+    /** @var int */
+    private $total_count = 0;
 
     /**
      * QueryResponseModel constructor.
@@ -14,7 +19,46 @@ class QueryResponseModel
      */
     public function __construct($model_list, $total_count)
     {
+        Assert::isArrayOfType($model_list, NormalizableInterface::class, 'model_list');
+        Assert::isNotNegative($total_count);
+
         $this->model_list = $model_list;
         $this->total_count = $total_count;
+    }
+
+    /**
+     * @return NormalizableInterface[]
+     */
+    public function getModelList()
+    {
+        return $this->model_list;
+    }
+
+    /**
+     * @param NormalizableInterface[] $model_list
+     * @return QueryResponseModel
+     */
+    public function setModelList($model_list)
+    {
+        $this->model_list = $model_list;
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getTotalCount()
+    {
+        return $this->total_count;
+    }
+
+    /**
+     * @param int $total_count
+     * @return QueryResponseModel
+     */
+    public function setTotalCount($total_count)
+    {
+        $this->total_count = $total_count;
+        return $this;
     }
 }
