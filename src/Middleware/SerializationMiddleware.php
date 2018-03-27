7d648800655731458a361c4e1b633ec133dd5612
Necip Allef <necipallef@gmail.com>
2018-03-27 14:58:31 +0300

    CHANGE remove before

--- src/Middleware/SerializationMiddleware.php
+++ src/Middleware/SerializationMiddleware.php
@@ -2,38 +2,14 @@
 
 namespace Fabstract\Component\REST\Middleware;
 
-use Fabstract\Component\Http\Constant\HttpMethods;
-use Fabstract\Component\Http\Exception\StatusCodeException\BadRequestException;
 use Fabstract\Component\Http\MiddlewareBase;
-use Fabstract\Component\Serializer\Exception\ParseException;
+use Fabstract\Component\REST\Constant\Services;
 
 class SerializationMiddleware extends MiddlewareBase
 {
-
-    public function before()
-    {
-        if ($this->requestCannotContainBody() === false &&
-            isset($this->serializer)
-        ) {
-            try {
-                $decoded = $this->serializer->getEncoder()->decode($this->request->getContent());
-                $this->request->setBody($decoded);
-            } catch (ParseException $exception) {
-                throw new BadRequestException();
-            }
-        }
-    }
-
-    private function requestCannotContainBody()
-    {
-        return $this->request->getMethod() === HttpMethods::GET ||
-            $this->request->getMethod() === HttpMethods::HEAD ||
-            $this->request->getMethod() === HttpMethods::OPTIONS;
-    }
-
     public function after()
     {
-        if (isset($this->serializer)) {
+        if ($this->getContainer()->has(Services::SERIALIZER)) {
             $serialized = $this->serializer->serialize($this->response->getReturnedValue());
             $this->response->setReturnedValue($serialized);
         }
