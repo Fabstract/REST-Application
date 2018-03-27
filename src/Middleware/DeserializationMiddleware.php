9c33ff9a07241999417a5d548a1d105bdae707ef
Necip Allef <necipallef@gmail.com>
2018-03-27 14:58:46 +0300

    CHANGE decode first then normalize

--- src/Middleware/DeserializationMiddleware.php
+++ src/Middleware/DeserializationMiddleware.php
@@ -2,11 +2,14 @@
 
 namespace Fabstract\Component\REST\Middleware;
 
+use Fabstract\Component\Http\Exception\StatusCodeException\BadRequestException;
 use Fabstract\Component\Http\MiddlewareBase;
+use Fabstract\Component\REST\Constant\Services;
+use Fabstract\Component\Serializer\Exception\ParseException;
 use Fabstract\Component\Serializer\Normalizer\Type;
 use Fabstract\Component\REST\Assert;
 
-class NormalizationMiddleware extends MiddlewareBase
+class DeserializationMiddleware extends MiddlewareBase
 {
     /** @var Type */
     private $type = null;
@@ -19,11 +22,14 @@ class NormalizationMiddleware extends MiddlewareBase
 
     public function before()
     {
-        if (isset($this->serializer)) {
-            $normalized = $this->serializer->getNormalizer()->denormalize(
-                $this->request->getBody(),
-                $this->type
-            );
+        if ($this->getContainer()->has(Services::SERIALIZER) === true) {
+            try {
+                $decoded = $this->serializer->getEncoder()->decode($this->request->getContent());
+            } catch (ParseException $exception) {
+                throw new BadRequestException();
+            }
+
+            $normalized = $this->serializer->getNormalizer()->denormalize($decoded, $this->type);
 
             $this->request->setBody($normalized);
         }
