55bed27fee99ca91c580077d4e73fc6be7087465
Necip Allef <necipallef@gmail.com>
2018-03-27 15:17:02 +0300

    CHANGE removed all methods and added only one setRequestBodyModel method with 2 params

--- src/Action.php
+++ src/Action.php
@@ -10,36 +10,19 @@ use Fabstract\Component\REST\Middleware\ValidationMiddleware;
 class Action extends \Fabstract\Component\Http\Action
 {
     /**
-     * @param RequestBodyModelBase $request_body_model
-     * @return Action
-     */
-    public function setRequestBodyModel($request_body_model)
-    {
-        return $this->setRequestBodyModelInternal($request_body_model);
-    }
-
-    /**
-     * @param RequestBodyModelBase $request_body_model
-     * @return Action
-     */
-    public function setRequestBodyModelArray($request_body_model)
-    {
-        return $this->setRequestBodyModelInternal($request_body_model, true);
-    }
-
-    /**
-     * @param RequestBodyModelBase $request_body_model
+     * @param string $request_body_model_class
      * @param bool $is_array
      * @return Action
      */
-    protected function setRequestBodyModelInternal($request_body_model, $is_array = false)
+    public function setRequestBodyModel($request_body_model_class, $is_array = false)
     {
-        Assert::isChildOf($request_body_model, RequestBodyModelBase::class, 'request body model');
+        Assert::isClassExists($request_body_model_class, 'request_body_model_class');
+        Assert::isChildOf($request_body_model_class, RequestBodyModelBase::class, 'request body model');
 
         if ($is_array === true) {
-            $type = new ArrayType($request_body_model);
+            $type = new ArrayType($request_body_model_class);
         } else {
-            $type = new Type($request_body_model);
+            $type = new Type($request_body_model_class);
         }
 
         return $this
